069fd1158f8b46744699e8a829169bb25c1abb49
AN <alializadehff@gmaill.com>
2024-12-15 23:48:59 -1200

    project_complete

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+    screens: {
+      "mobile":"300px",
+      "desktop": "1000px"
+    }
+  },
+  plugins: [],
+}
+
